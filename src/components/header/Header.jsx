b8668b1dff8a2808fec7f2c4147fcc2dc4789eda
deepakkr <deepaksinghofficial2224@gmail.com>
2024-02-01 13:40:15 +0530

    c

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import  { useState, useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
@@ -11,127 +11,115 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/movix-logo.svg";
 import "./style.scss";
 const Header = () => {
-    const [show, setShow] = useState("top");
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [query, setQuery] = useState("");
-    const [showSearch, setShowSearch] = useState("");
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    useEffect(()=>{
-      window.scrollTo(0,0);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
-    },[location])
-
-    const controlNavbar=()=>{
-      if(window.scrollY>200){
-        if(window.scrollY>lastScrollY && !mobileMenu)
-        {
-          setShow("hide")
-        }else{
-          setShow("show")
-        }
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
       }
-      else{
-        setShow("top")
-      }
-       setLastScrollY(window.scrollY);
+    } else {
+      setShow("top");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
     };
+  }, [lastScrollY]);
 
-    useEffect(()=>{
-        window.addEventListener("scroll",controlNavbar);
-        return ()=>{
-          window.removeEventListener("scroll",controlNavbar);
-
-        }
-    },[lastScrollY])
-
-    const searcjQueryHandler = (event) => {
-      if (event.key === "Enter" && query.length > 0) {
-        navigate(`/search/${query}`);
-        setTimeout(()=>{
-            setShowSearch(false);
-        },1000)
-      }
-    };
-
-
-    const openSearch=()=>{
-      setMobileMenu(false);
-      setShowSearch(true);
-      
+  const searcjQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
     }
-    const openMobileMenu=()=>{
-      setMobileMenu(true);
-      setShowSearch(false);
+  };
+
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  const navigationHAndler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else if (type == "tv") {
+      navigate("/explore/tv");
+    } else if (type == "signup") {
+      navigate("/signup");
+    } else {
+      navigate("/login");
     }
-
-    const navigationHAndler=(type)=>{
-      if(type==="movie")
-      {
-        navigate("/explore/movie");
-
-      }
-      else if(type=="tv") {
-        navigate("/explore/tv");
-      }
-      else if(type=="signup"){
-        navigate("/signup");
-      }
-      else{
-        navigate("/login");
-      }
-      setMobileMenu(false);
-
-    }
+    setMobileMenu(false);
+  };
   return (
     <>
-    <header className={`header ${mobileMenu ? "mobileView":""} ${show}`}>
+      <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
         <ContentWrapper>
-            <div className="logo" onClick={()=>navigate("/")}>
-                <img src={logo} alt="" />
-            </div>
-            <ul className="menuItems">
-              <li className='menuItem' onClick={()=>navigationHAndler("movie")}>Movies</li>
-              <li className='menuItem' onClick={()=>navigationHAndler("tv")}>TV Shows</li>
-              <li className='menuItem'>
-                <HiOutlineSearch onClick={openSearch}/>
-              </li>&nbsp;&nbsp;&nbsp;
-              <li className='menuItem'onClick={()=>navigationHAndler("login")}>Login</li>
-              <li className='menuItem' onClick={()=>navigationHAndler("signup")}>Signup</li>
-                
-            </ul>
+          <div className="logo" onClick={() => navigate("/")}>
+            <img src={logo} alt="" />
+          </div>
+          <ul className="menuItems">
+            <li className="menuItem" onClick={() => navigationHAndler("movie")}>
+              Movies
+            </li>
+            <li className="menuItem" onClick={() => navigationHAndler("tv")}>
+              TV Shows
+            </li>
+            <li className="menuItem">
+              <HiOutlineSearch onClick={openSearch} />
+            </li>
+            &nbsp;&nbsp;&nbsp;
+          </ul>
 
-            <div className="mobileMenuItems">
-            <HiOutlineSearch onClick={openSearch}/>
-            {
-              mobileMenu ? (
-                <VscChromeClose onClick={()=>setMobileMenu(false)}/>
-              ) : (
-                <SlMenu onClick={openMobileMenu} />
-              )
-            }
-            
-            
-            </div>
+          <div className="mobileMenuItems">
+            <HiOutlineSearch onClick={openSearch} />
+            {mobileMenu ? (
+              <VscChromeClose onClick={() => setMobileMenu(false)} />
+            ) : (
+              <SlMenu onClick={openMobileMenu} />
+            )}
+          </div>
         </ContentWrapper>
-        { showSearch && <div className="searchBar">
-          <ContentWrapper>
-          <div className="searchInput">
-              <input
-                type="text"
-                onChange={(e) => setQuery(e.target.value)}
-                onKeyUp={searcjQueryHandler}
-                placeholder="Search For Movies or TV Shows..."
-              />
-              <VscChromeClose onClick={()=>setShowSearch(false)}/>
-            </div>
-          </ContentWrapper>
-        </div>}
-    </header>
+        {showSearch && (
+          <div className="searchBar">
+            <ContentWrapper>
+              <div className="searchInput">
+                <input
+                  type="text"
+                  onChange={(e) => setQuery(e.target.value)}
+                  onKeyUp={searcjQueryHandler}
+                  placeholder="Search For Movies or TV Shows..."
+                />
+                <VscChromeClose onClick={() => setShowSearch(false)} />
+              </div>
+            </ContentWrapper>
+          </div>
+        )}
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
